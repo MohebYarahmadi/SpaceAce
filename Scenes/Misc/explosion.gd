48ed8cb66bd199b5f06ad6ba9757f7145a62950b
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-05-15 18:02:16 +0330

    nc
    update

--- Scenes/Misc/explosion.gd
+++ Scenes/Misc/explosion.gd
@@ -1,9 +1,22 @@
 extends Node2D
+class_name Explosion
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const BOOM: String = "boom"
+const EXPLODE: String = "explode"
+
+var _anim_name: String = EXPLODE
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animated_sprite_2d.animation = _anim_name
+	animated_sprite_2d.play()
+
+
+func setup(anim_name: String) -> void:
+	_anim_name = anim_name
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
