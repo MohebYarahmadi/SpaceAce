b07e4afb62900f479e3098b88a692eceed6ff38d
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-05-16 11:29:59 +0330

    Powerup Added

--- Globals/SignalHub.gd
+++ Globals/SignalHub.gd
@@ -4,15 +4,25 @@ extends Node
 signal on_player_hit(v: int)
 signal on_score_updated(v: int)
 signal on_create_explosion(pos: Vector2, anim_name: String)
+signal on_create_powerup(pos: Vector2, pu_type: PowerUp.PowerUpType)
+signal on_create_powerup_random(pos: Vector2)
 
 
 func emit_on_player_hit(v: int) -> void:
 	on_player_hit.emit(v)
 
 
-func emit_on_score_updated(v: int):
+func emit_on_score_updated(v: int) -> void:
 	on_score_updated.emit(v)
 
 
-func emit_on_create_explosion(pos: Vector2, anim_name: String):
+func emit_on_create_explosion(pos: Vector2, anim_name: String) -> void:
 	on_create_explosion.emit(pos, anim_name)
+
+
+func emit_on_create_powerup(pos: Vector2, pu_type: PowerUp.PowerUpType) -> void:
+	on_create_powerup.emit(pos, pu_type)
+
+
+func emit_on_create_powerup_random(pos: Vector2) -> void:
+	on_create_powerup_random.emit(pos)
--- Scenes/ObjectMaker/ObjectMaker.gd
+++ Scenes/ObjectMaker/ObjectMaker.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const EXPLOSION = preload("uid://32ovesy8b5fc")
+const POWER_UP = preload("uid://cngtd7wacyhf7")
 
 const ADD_OBJECT: String = "add_object"
 
@@ -8,6 +9,8 @@ const ADD_OBJECT: String = "add_object"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalHub.on_create_explosion.connect(create_explosion)
+	SignalHub.on_create_powerup.connect(create_powerup)
+	SignalHub.on_create_powerup_random.connect(create_powerup_random)
 
 
 func add_object(obj: Node, pos: Vector2) -> void:
@@ -19,3 +22,15 @@ func create_explosion(pos: Vector2, anim_name: String) -> void:
 	var scene: Explosion = EXPLOSION.instantiate()
 	scene.setup(anim_name)
 	call_deferred(ADD_OBJECT, scene, pos)
+
+
+func create_powerup(pos: Vector2, pu_type: PowerUp.PowerUpType) -> void:
+	var pu: PowerUp = POWER_UP.instantiate()
+	pu.power_up_type = pu_type
+	call_deferred(ADD_OBJECT, pu, pos)
+
+
+func create_powerup_random(pos: Vector2) -> void:
+	var rpu: PowerUp.PowerUpType = PowerUp.PowerUpType.values().pick_random()
+	create_powerup(pos, rpu)
+	
