8651fb2f4eb4ff5e43d67ec7892ce4c3c3152fb0
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-05-16 09:05:29 +0330

    Explosion added to level

--- Globals/SignalHub.gd
+++ Globals/SignalHub.gd
@@ -3,6 +3,7 @@ extends Node
 
 signal on_player_hit(v: int)
 signal on_score_updated(v: int)
+signal on_create_explosion(pos: Vector2, anim_name: String)
 
 
 func emit_on_player_hit(v: int) -> void:
@@ -11,3 +12,7 @@ func emit_on_player_hit(v: int) -> void:
 
 func emit_on_score_updated(v: int):
 	on_score_updated.emit(v)
+
+
+func emit_on_create_explosion(pos: Vector2, anim_name: String):
+	on_create_explosion.emit(pos, anim_name)
--- Scenes/ObjectMaker/ObjectMaker.gd
+++ Scenes/ObjectMaker/ObjectMaker.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
+const EXPLOSION = preload("uid://32ovesy8b5fc")
 
 const ADD_OBJECT: String = "add_object"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	SignalHub.on_create_explosion.connect(create_explosion)
 
 
 func add_object(obj: Node, pos: Vector2) -> void:
 	add_child(obj)
 	obj.global_position = pos
+
+
+func create_explosion(pos: Vector2, anim_name: String) -> void:
+	var scene: Explosion = EXPLOSION.instantiate()
+	scene.setup(anim_name)
+	call_deferred(ADD_OBJECT, scene, pos)
