ebf7ff0ced103f07c71468923fa903e1158ff674
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-05-23 09:30:58 +0330

    Player Health Update

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -27,4 +27,8 @@ func _on_area_entered(area: Area2D) -> void:
 		match area.power_up_type:
 			PowerUp.PowerUpType.Shield:
 				shield.enable_shield()
+	elif area is Projectile:
+		SignalHub.emit_on_player_hit(area.get_damage())
+			
+			
 				
--- Scenes/Ui/gameUi.gd
+++ Scenes/Ui/gameUi.gd
@@ -1,8 +1,15 @@
 extends Control
 
-func _ready() -> void:
-	pass
+@onready var health_bar: HealthBar = $ColorRect/MarginContainer/HealthBar
+
+
+func _enter_tree() -> void:
+	SignalHub.on_player_hit.connect(player_hit)
+
+
+func player_hit(v: int) -> void:
+	health_bar.take_damage(v)
 
 
 func _on_health_bar_died() -> void:
-	print("Died")
+	print("Player DIED")
