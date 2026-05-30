9b868e184d7eed9ee7e03002ccfd10938d493581
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-05-30 09:47:00 +0330

    GameUI Score Lable Updated

--- Scenes/Ui/gameUi.gd
+++ Scenes/Ui/gameUi.gd
@@ -2,11 +2,13 @@ extends Control
 
 @onready var health_bar: HealthBar = $ColorRect/MarginContainer/HealthBar
 @onready var sound: AudioStreamPlayer2D = $Sound
+@onready var score_label: Label = $ColorRect/MarginContainer/ScoreLabel
 
 
 func _enter_tree() -> void:
 	SignalHub.on_player_hit.connect(player_hit)
 	SignalHub.on_player_health_bonus.connect(health_bonus)
+	SignalHub.on_score_updated.connect(score_updated)
 
 
 func player_hit(v: int) -> void:
@@ -16,6 +18,10 @@ func player_hit(v: int) -> void:
 func health_bonus(v: int) -> void:
 	health_bar.incr_value(v)
 	sound.play()
+	
+
+func score_updated(v: int) -> void:
+	score_label.text = "%06d" % v
 
 
 func _on_health_bar_died() -> void:
